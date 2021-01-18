3a9e6008377e70c4403b05f5310d2e0b7f04f450
Eric Busboom <eric@civicknowledge.com>
2021-01-17 17:13:24 -0800

    .

--- src/publicdata/census/__init__.py
+++ src/publicdata/census/__init__.py
@@ -4,6 +4,18 @@ from .appurl import CensusUrl
 from .files.appurl import CensusFileUrl, CensusGeoUrl
 from .censusreporter.url import CensusReporterUrl, CensusReporterShapeURL
 
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = 'publicdata.census'
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+
 def census_table(table, state, sl='state', year=2018, release=5):
     import rowgenerators as rg
     return rg.dataframe(f'census://{year}/{release}/{state}/{sl}/{table}')
